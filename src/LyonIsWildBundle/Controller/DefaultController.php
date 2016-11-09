b69d5febce1d783f1bbd3f92beea1989da2a4648
tanguilb <tangui.lebourdonnec@gmail.com>
2016-11-09 20:41:20 +0100

    add a function to uploads database

--- src/LyonIsWildBundle/Controller/DefaultController.php
+++ src/LyonIsWildBundle/Controller/DefaultController.php
@@ -2,12 +2,46 @@
 
 namespace LyonIsWildBundle\Controller;
 
+use LyonIsWildBundle\Entity\Place;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('LyonIsWildBundle:Default:index.html.twig');
+    }
+
+    public function loadAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $url = 'json/rdata.json';
+        $contents = file_get_contents($url);
+        $results = json_decode($contents, true);
+        $sites = $results["features"];
+        foreach ($sites as $site){
+                $siteData = new Place();
+                $siteData->setType($site["properties"]["type"]);
+                $siteData->setName($site["properties"]["nom"]);
+                $siteData->setAddress($site["properties"]["adresse"]);
+                $siteData->setPostal($site["properties"]["codepostal"]);
+                $siteData->setCity($site["properties"]["commune"]);
+                $siteData->setPhonenumber($site["properties"]["telephone"]);
+                $siteData->setEmail($site["properties"]["email"]);
+                $siteData->setWebsite($site["properties"]["siteweb"]);
+                $siteData->setFecebook($site["properties"]["facebook"]);
+                $siteData->setLongitude($site["geometry"]["coordinates"][0]);
+                $siteData->setLatitude($site["geometry"]["coordinates"][1]);
+                $em->persist($siteData);
+
+        }
+        $em->flush();
+
+
+        return $em;
+
+
     }
 }
---
+++ src/LyonIsWildBundle/Entity/Place.php
@@ -0,0 +1,344 @@
+<?php
+
+namespace LyonIsWildBundle\Entity;
+
+/**
+ * Place
+ */
+class Place
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * @var string
+     */
+    private $postal;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var int
+     */
+    private $phonenumber;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $website;
+
+    /**
+     * @var string
+     */
+    private $fecebook;
+
+    /**
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     */
+    private $latitude;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Place
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Place
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Place
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set postal
+     *
+     * @param string $postal
+     *
+     * @return Place
+     */
+    public function setPostal($postal)
+    {
+        $this->postal = $postal;
+
+        return $this;
+    }
+
+    /**
+     * Get postal
+     *
+     * @return string
+     */
+    public function getPostal()
+    {
+        return $this->postal;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Place
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set phonenumber
+     *
+     * @param integer $phonenumber
+     *
+     * @return Place
+     */
+    public function setPhonenumber($phonenumber)
+    {
+        $this->phonenumber = $phonenumber;
+
+        return $this;
+    }
+
+    /**
+     * Get phonenumber
+     *
+     * @return int
+     */
+    public function getPhonenumber()
+    {
+        return $this->phonenumber;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Place
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set website
+     *
+     * @param string $website
+     *
+     * @return Place
+     */
+    public function setWebsite($website)
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+    /**
+     * Get website
+     *
+     * @return string
+     */
+    public function getWebsite()
+    {
+        return $this->website;
+    }
+
+    /**
+     * Set fecebook
+     *
+     * @param string $fecebook
+     *
+     * @return Place
+     */
+    public function setFecebook($fecebook)
+    {
+        $this->fecebook = $fecebook;
+
+        return $this;
+    }
+
+    /**
+     * Get fecebook
+     *
+     * @return string
+     */
+    public function getFecebook()
+    {
+        return $this->fecebook;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     *
+     * @return Place
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     *
+     * @return Place
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+}
